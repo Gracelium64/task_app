2a627408831387267e4342db55b606d0a8003a15
Grace <grace6424@icloud.com>
2025-04-02 12:31:18 +0200

    4.5.1.1 ✅ 4.5.2.1 ✅ 4.5.2.2 ✅

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
@@ -4,6 +4,17 @@ class S4521 extends StatelessWidget {
   const S4521({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return HelloWorld2();
+  }
+}
+
+class HelloWorld2 extends StatelessWidget {
+  const HelloWorld2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Hello World'),
+    );
   }
 }
