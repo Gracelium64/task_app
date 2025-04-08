b4ea7b373f4027f7853e9992e4fcfed41c30142c
Grace <grace6424@icloud.com>
2025-04-08 12:25:48 +0200

    4.5.2.3 ✅ 4.5.2.4 ✅

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
@@ -4,6 +4,34 @@ class S4523 extends StatelessWidget {
   const S4523({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyNameWidget();
+  }
+}
+
+class MyNameWidget extends StatefulWidget {
+  const MyNameWidget({super.key});
+
+  @override
+  State<MyNameWidget> createState() => _MyNameWidgetState();
+}
+
+class _MyNameWidgetState extends State<MyNameWidget> {
+  String placeHolder = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(placeHolder),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              placeHolder = 'Grace';
+            });
+          },
+          child: Text('Name anzeigen')
+          ),
+      ],
+    );
   }
 }
--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -4,6 +4,50 @@ class S4524 extends StatelessWidget {
   const S4524({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ShowHideNameWidget();
   }
 }
+
+class ShowHideNameWidget extends StatefulWidget {
+  const ShowHideNameWidget({super.key});
+
+  @override
+  State<ShowHideNameWidget> createState() => _ShowHideNameWidgetState();
+}
+
+class _ShowHideNameWidgetState extends State<ShowHideNameWidget> {
+  String nameString = '';
+  String buttonString = 'Name anzeigen';
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(nameString),
+        ElevatedButton(
+        onPressed: () {
+          setState(() {
+            clickMe();
+          });
+        },
+        child: Text(buttonString),
+        ),
+      ],
+    );
+  }
+
+  void clickMe() {
+     if (nameString == '') {
+      nameString = 'Grace';
+    } else {
+      nameString = '';
+    }
+    
+    if (buttonString == 'Name anzeigen') {
+      buttonString = 'Name verstecken';
+    } else {
+      buttonString = 'Name anzeigen';
+    }
+  }
+}
+
